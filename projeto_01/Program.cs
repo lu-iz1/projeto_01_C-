d819f330fa13b018d7b268f34f3cc9a727f9b7c5
Luiz Eduardo Kauan Da Silva <Luiz.trindade2002@outlook.com>
2021-09-08 21:49:44 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projeto_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aluno;
+            double nota1, nota2, media;
+
+            Console.WriteLine("Digite o nome do Aluno: ");
+            aluno = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 6)
+                Console.WriteLine("A média do aluno " + aluno + " foi " + media + " Está Aprovado");
+            else
+                Console.WriteLine("A média do aluno " + aluno + " foi " + media + "Está Reprovado");
+        }
+    }
+}
